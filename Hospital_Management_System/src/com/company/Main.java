c4a362bfb60df94e73f118e6cf82efb9d095eabb
UmmeHani1513 <humme280@gmail.com>
2025-04-23 15:37:42 +0500

    without

--- Hospital_Management_System/src/com/company/Main.java
+++ Hospital_Management_System/src/com/company/Main.java
@@ -2,101 +2,55 @@ package com.company;
 
 import java.util.Scanner;
 
-// Strategy interface
-interface ModuleStrategy {
-    void execute();
-}
-
-// Admin Module Strategy
-class AdminStrategy implements ModuleStrategy {
-    public void execute() {
-        Admin admin = new Admin();
-        admin.choose();
-    }
-}
-
-// Department Module Strategy
-class DepartmentStrategy implements ModuleStrategy {
-    public void execute() {
-        Dep department = new Dep();
-        department.Dep_Display();
-    }
-}
-
-// Pharmacy Module Strategy
-class PharmacyStrategy implements ModuleStrategy {
-    public void execute() {
-        Pharmacy pharmacy = new Pharmacy();
-        pharmacy.display();
-    }
-}
-
-// Context Class - the context class will interact with the strategies
-class ModuleContext {
-    private ModuleStrategy moduleStrategy;
-
-    public void setModuleStrategy(ModuleStrategy moduleStrategy) {
-        this.moduleStrategy = moduleStrategy;
-    }
-
-    public void executeModule() {
-        this.moduleStrategy.execute();
-    }
-}
-
-// Main Class with login and menu system
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int a, y;
-        String u_N, password;
+        int choice, exitFlag;
+        String username, password;
 
         System.out.println("**********************************************************************");
         System.out.println("\tTo Access Hospital System Enter LOGIN:");
         System.out.println("----------------------------------------------------------------------");
         System.out.print("\tUSER NAME:\t");
-        u_N = input.next();
+        username = input.next();
         System.out.print("\tPASSWORD:\t");
         password = input.next();
         System.out.println("----------------------------------------------------------------------");
         System.out.println("**********************************************************************");
 
         // Check user credentials
-        if (u_N.equals("project") && password.equals("laaa")) {
+        if (username.equals("project") && password.equals("laaa")) {
             do {
                 System.out.println("----------------------------------------------------------------------");
                 System.out.println("          Welcome To Hospital Management System");
-                System.out.println("-----------------------------------------------------------------------");
-                System.out.println("1.ADMIN\t2.DEPARTMENTS\t3.PHARMACY\n");
-                System.out.println("Enter option:");
-                a = input.nextInt();
+                System.out.println("----------------------------------------------------------------------");
+                System.out.println("1. ADMIN\t2. DEPARTMENTS\t3. PHARMACY");
+                System.out.print("Enter option: ");
+                choice = input.nextInt();
 
-                // Strategy pattern context for dynamic module selection
-                ModuleContext context = new ModuleContext();
-
-                switch (a) {
+                switch (choice) {
                     case 1:
-                        context.setModuleStrategy(new AdminStrategy());
-                        context.executeModule();
+                        Admin admin = new Admin();
+                        admin.choose();
                         break;
                     case 2:
-                        context.setModuleStrategy(new DepartmentStrategy());
-                        context.executeModule();
+                        Dep department = new Dep();
+                        department.Dep_Display();
                         break;
                     case 3:
-                        context.setModuleStrategy(new PharmacyStrategy());
-                        context.executeModule();
+                        Pharmacy pharmacy = new Pharmacy();
+                        pharmacy.display();
                         break;
                     default:
-                        System.out.println("Enter Valid Option From (1, 2, or 3)");
+                        System.out.println("Enter a valid option (1, 2, or 3)");
                         break;
                 }
 
-                System.out.println("To Exit System Press 1");
-                y = input.nextInt();
-            } while (y != 1);
+                System.out.println("To Exit System Press 1, or press any other number to continue:");
+                exitFlag = input.nextInt();
+            } while (exitFlag != 1);
         } else {
-            System.out.println("***WRONG!!!!!!!USER NAME OR PASSWORD*****");
+            System.out.println("*** WRONG USERNAME OR PASSWORD ***");
         }
     }
 }
--- Hospital_Management_System/src/com/company/Pharmacy.java
+++ Hospital_Management_System/src/com/company/Pharmacy.java
@@ -1,359 +1,108 @@
 package com.company;
+
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.io.File;
 import java.io.IOException;
-// Command Interface
-interface Command {
-    void execute();
-}
+import java.util.Scanner;
 
-// Concrete Command Classes for Adding Medicines
-class AddCardiacMedicineCommand implements Command {
-    private Cardiac_med cardiacMed;
-
-    public AddCardiacMedicineCommand(Cardiac_med cardiacMed) {
-        this.cardiacMed = cardiacMed;
-    }
-
-    @Override
-    public void execute() {
-        cardiacMed.add();
-    }
-}
-
-class AddNeurologyMedicineCommand implements Command {
-    private Neurology_med neurologyMed;
-
-    public AddNeurologyMedicineCommand(Neurology_med neurologyMed) {
-        this.neurologyMed = neurologyMed;
-    }
-
-    @Override
-    public void execute() {
-        neurologyMed.add();
-    }
-}
-
-class AddEyeMedicineCommand implements Command {
-    private Eye_med eyeMed;
-
-    public AddEyeMedicineCommand(Eye_med eyeMed) {
-        this.eyeMed = eyeMed;
-    }
-
-    @Override
-    public void execute() {
-        eyeMed.add();
-    }
-}
-
-class AddDentalMedicineCommand implements Command {
-    private Dental_med dentalMed;
-
-    public AddDentalMedicineCommand(Dental_med dentalMed) {
-        this.dentalMed = dentalMed;
-    }
-
-    @Override
-    public void execute() {
-        dentalMed.add();
-    }
-}
-
-// Concrete Command Classes for Searching Medicines
-class SearchCardiacMedicineCommand implements Command {
-    private search_med searchMed;
-
-    public SearchCardiacMedicineCommand(search_med searchMed) {
-        this.searchMed = searchMed;
-    }
-
-    @Override
-    public void execute() {
-        searchMed.search_heart();
-    }
-}
-
-class SearchNeurologyMedicineCommand implements Command {
-    private search_med searchMed;
-
-    public SearchNeurologyMedicineCommand(search_med searchMed) {
-        this.searchMed = searchMed;
-    }
-
-    @Override
-    public void execute() {
-        searchMed.search_Brain_Med();
-    }
-}
-
-class SearchEyeMedicineCommand implements Command {
-    private search_med searchMed;
-
-    public SearchEyeMedicineCommand(search_med searchMed) {
-        this.searchMed = searchMed;
-    }
-
-    @Override
-    public void execute() {
-        searchMed.search_Eye_Med();
-    }
-}
-
-class SearchDentalMedicineCommand implements Command {
-    private search_med searchMed;
-
-    public SearchDentalMedicineCommand(search_med searchMed) {
-        this.searchMed = searchMed;
-    }
-
-    @Override
-    public void execute() {
-        searchMed.search_Dental_Med();
-    }
-}
-
-// The Pharmacy Class (Invoker)
 public class Pharmacy {
-    private Command addCommand;
-    private Command searchCommand;
-
-    public void setAddCommand(Command command) {
-        this.addCommand = command;
-    }
-
-    public void setSearchCommand(Command command) {
-        this.searchCommand = command;
-    }
 
     public void display() {
         int c, a, n;
         Scanner input = new Scanner(System.in);
+
         do {
             System.out.println("*******  PHARMACY  *********");
-            System.out.println("1.Add New Medicine\t2.Search Medicine List\nEnter Option:\t");
+            System.out.println("1. Add New Medicine\t2. Search Medicine List");
+            System.out.print("Enter Option:\t");
             c = input.nextInt();
-            Cardiac_med cardiac = new Cardiac_med();
-            Neurology_med neuro = new Neurology_med();
-            Eye_med e = new Eye_med();
-            Dental_med den = new Dental_med();
-            search_med h = new search_med();
 
             switch (c) {
                 case 1:
-                    System.out.println("Add Medicinal Drug In:\n1.Cardiology\n2.Neurology\n3.Eye\n4.Dental");
+                    System.out.println("Add Medicinal Drug In:\n1. Cardiology\n2. Neurology\n3. Eye\n4. Dental");
                     a = input.nextInt();
                     switch (a) {
                         case 1:
-                            setAddCommand(new AddCardiacMedicineCommand(cardiac));
-                            addCommand.execute();
+                            addMedicine("Cardiac_Medicines.txt");
                             break;
                         case 2:
-                            setAddCommand(new AddNeurologyMedicineCommand(neuro));
-                            addCommand.execute();
+                            addMedicine("Brain_Medicines.txt");
                             break;
                         case 3:
-                            setAddCommand(new AddEyeMedicineCommand(e));
-                            addCommand.execute();
+                            addMedicine("Eye_Medicines.txt");
                             break;
                         case 4:
-                            setAddCommand(new AddDentalMedicineCommand(den));
-                            addCommand.execute();
+                            addMedicine("Dental_Medicines.txt");
                             break;
+                        default:
+                            System.out.println("Invalid option.");
                     }
                     break;
+
                 case 2:
-                    System.out.println("View/Search Medicines of:\n1.Cardiology\n2.Neurology\n3.Eye\n4.Dental");
+                    System.out.println("Search Medicines in:\n1. Cardiology\n2. Neurology\n3. Eye\n4. Dental");
                     a = input.nextInt();
                     switch (a) {
                         case 1:
-                            setSearchCommand(new SearchCardiacMedicineCommand(h));
-                            searchCommand.execute();
+                            searchMedicine("Cardiac_Medicines.txt");
                             break;
                         case 2:
-                            setSearchCommand(new SearchNeurologyMedicineCommand(h));
-                            searchCommand.execute();
+                            searchMedicine("Brain_Medicines.txt");
                             break;
                         case 3:
-                            setSearchCommand(new SearchEyeMedicineCommand(h));
-                            searchCommand.execute();
+                            searchMedicine("Eye_Medicines.txt");
                             break;
                         case 4:
-                            setSearchCommand(new SearchDentalMedicineCommand(h));
-                            searchCommand.execute();
+                            searchMedicine("Dental_Medicines.txt");
                             break;
+                        default:
+                            System.out.println("Invalid option.");
                     }
                     break;
+
+                default:
+                    System.out.println("Invalid choice.");
             }
-            System.out.println("To Exit Pharmacy press 0");
+
+            System.out.println("To Exit Pharmacy press 0, any other key to continue:");
             n = input.nextInt();
         } while (n != 0);
     }
-}
 
-// Abstract Class for Adding Medicines
-abstract class ADD_Medicine extends Pharmacy {
-    String med_name, exp_date, used_for;
-    float cost;
-    public abstract void add();
-}
-
-// Concrete Classes for Different Categories of Medicines
-class Cardiac_med extends ADD_Medicine {
-    public void add() {
+    private void addMedicine(String fileName) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter Medicine Name:");
-        med_name = input.next();
+        String medName = input.next();
         System.out.println("Cost:");
-        cost = input.nextInt();
-        System.out.println("Expiry Date:\t");
-        exp_date = input.next();
+        float cost = input.nextFloat();
+        System.out.println("Expiry Date:");
+        String expDate = input.next();
         System.out.println("Used for curing:");
-        used_for = input.next();
-        try {
-            FileWriter g = new FileWriter("Cardiac_Medicines.txt", true);
-            g.write(med_name + "\t" + cost + "\t\t" + exp_date + "\t" + used_for + "\n");
-            System.out.println("Written");
-            g.close();
+        String usedFor = input.next();
+
+        try (FileWriter writer = new FileWriter(fileName, true)) {
+            writer.write(medName + "\t" + cost + "\t\t" + expDate + "\t" + usedFor + "\n");
+            System.out.println("Medicine added successfully.");
         } catch (IOException e) {
-            System.out.println("Error ");
+            System.out.println("Error writing to file.");
             e.printStackTrace();
         }
     }
-}
 
-class Neurology_med extends ADD_Medicine {
-    public void add() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter Medicine Name:");
-        med_name = input.next();
-        System.out.println("Cost:");
-        cost = input.nextInt();
-        System.out.println("Expiry Date:\t");
-        exp_date = input.next();
-        System.out.println("Used for curing:");
-        used_for = input.next();
-        try {
-            FileWriter g = new FileWriter("Brain_Medicines.txt", true);
-            g.write(med_name + "\t" + cost + "\t\t" + exp_date + "\t" + used_for + "\n");
-            System.out.println("Written");
-            g.close();
-        } catch (IOException e) {
-            System.out.println("Error ");
-            e.printStackTrace();
-        }
-    }
-}
-
-class Eye_med extends ADD_Medicine {
-    public void add() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter Medicine Name:");
-        med_name = input.next();
-        System.out.println("Cost:");
-        cost = input.nextInt();
-        System.out.println("Expiry Date:\t");
-        exp_date = input.next();
-        System.out.println("Used for curing:");
-        used_for = input.next();
-        try {
-            FileWriter g = new FileWriter("Eye_Medicines.txt", true);
-            g.write(med_name + "\t" + cost + "\t\t" + exp_date + "\t" + used_for + "\n");
-            System.out.println("Written");
-            g.close();
-        } catch (IOException e) {
-            System.out.println("Error ");
-            e.printStackTrace();
-        }
-    }
-}
-
-class Dental_med extends ADD_Medicine {
-    public void add() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter Medicine Name:");
-        med_name = input.next();
-        System.out.println("Cost:");
-        cost = input.nextInt();
-        System.out.println("Expiry Date:\t");
-        exp_date = input.next();
-        System.out.println("Used for curing:");
-        used_for = input.next();
-        try {
-            FileWriter g = new FileWriter("Dental_Medicines.txt", true);
-            g.write(med_name + "\t" + cost + "\t\t" + exp_date + "\t" + used_for + "\n");
-            System.out.println("Written");
-            g.close();
-        } catch (IOException e) {
-            System.out.println("Error ");
-            e.printStackTrace();
-        }
-    }
-}
-
-// Search Class
-class search_med extends Pharmacy {
-    ArrayList<String> Med = new ArrayList<String>();
-    ArrayList<String> Neuro = new ArrayList<String>();
-    ArrayList<String> Eye = new ArrayList<String>();
-    ArrayList<String> Dental = new ArrayList<String>();
-
-    public void search_heart() {
-        try {
-            File myObj = new File("Cardiac_Medicines.txt");
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
+    private void searchMedicine(String fileName) {
+        try (Scanner fileReader = new Scanner(new File(fileName))) {
+            System.out.println("Medicines listed in " + fileName + ":");
+            while (fileReader.hasNextLine()) {
+                System.out.println(fileReader.nextLine());
             }
-            myReader.close();
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            System.out.println("No records found for this department.");
         }
     }
 
-    public void search_Brain_Med() {
-        try {
-            File myObj = new File("Brain_Medicines.txt");
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
-            }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void search_Eye_Med() {
-        try {
-            File myObj = new File("Eye_Medicines.txt");
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
-            }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void search_Dental_Med() {
-        try {
-            File myObj = new File("Dental_Medicines.txt");
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
-            }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) {
+        Pharmacy p = new Pharmacy();
+        p.display();
     }
 }
